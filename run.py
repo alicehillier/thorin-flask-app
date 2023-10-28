35c3f39516e411a4bf23fc2dfafaab8b218fe7b9
Alice Hillier <alice_hillier@hotmail.com>
2023-10-28 10:26:46 +0100

    Writes code for routing.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+import json
 
 # name is the built in variable for name of the app
 app = Flask(__name__)
@@ -13,17 +14,34 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    # r means read only.
+    # assigns contents of file to variable json_data
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        # assign new variable called company, which contains the contents of json file, to send to html template
+    return render_template("about.html", page_title="About", company=data)
+
+@app.route("/about/<character_name>")
+def about_character(character_name):
+    character = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == character_name:
+                character = obj
+                # First character is variable passed into html file, second character is second variable created in this function
+    return render_template("character.html", character=character)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 # __main__ is the default module
